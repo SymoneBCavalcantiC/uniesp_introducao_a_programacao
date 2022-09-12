c9f149837e97a9459065f9ba5ca75bcb26692a88
Symone Cavalcanti <112357761+SymoneBCavalcantiC@users.noreply.github.com>
2022-09-12 06:40:55 -0300

    Delete Aula 5 060922 directory

--- Aula 5 060922/TED q3 060922.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Digite um número: "))
-
-resultado = ((((num + 1357) * 8) / 5) ** 2)
-
-print(f"{resultado :.2f}")
--- Aula 5 060922/TED q4 060922.py
+++
@@ -1,3 +0,0 @@
-nome = input("Digite seu nome: ")
-
-print(f"Você está convidado(a) à festa, {nome.upper()}!")
