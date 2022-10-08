b199863af7040ab6a3ae6e82a753321ab634710d
Symone Cavalcanti <112357761+SymoneBCavalcantiC@users.noreply.github.com>
2022-10-08 20:20:05 -0300

    Resposta AtivSala

--- Aula 9 041022/Atividade 041022/Q1.py
+++ Aula 9 041022/Atividade 041022/Q1.py
@@ -4,6 +4,9 @@
 # Por fim, mostre cada informação armazenada em seu dicionário.
 
 dados = {"primeiro_nome": "Willian", "sobrenome": "Pereira", "idade": 26, "cidade": "Starkville"}
+print(dados.keys())
+print(dados.values())
+print(dados.items())
 
 for k,v in dados.items():
-    print(f"A chave deste item é: {k}; e o valor é: {v}.")
+    print(f"A chave e o valor deste item são: {k} e {v}.")
